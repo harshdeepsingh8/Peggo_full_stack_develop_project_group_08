cd606290330694bfaf469ba0a31156ec1686aa61
Harshdeep Singh <harshdeepsingh8>
2026-09-25 09:46:41 -0500

    feat: add Lab 1.2 React header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header>
+      <img
+        src="https://itsm-ace.ca/images/logo.svg"
+        alt="Pixell River Financial logo"
+      />
+
+      <div>
+        <h1>Pixell River Employee Directory</h1>
+        <p>Welcome! View our employees and departments below.</p>
+      </div>
+    </header>
+  );
+}
